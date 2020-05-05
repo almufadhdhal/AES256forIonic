d3373b437f8d662c4871873335b97260ec3b901d
almufadhdhal abdulloh <almufadhdhal@gmail.com>
2020-05-05 09:58:37 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,30 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AES256 } from '@ionic-native/aes-256';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+  MyApp,
+  HomePage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+  BrowserModule,
+  IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+  MyApp,
+  HomePage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  StatusBar,
+  SplashScreen,
+  AES256,
+  {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { AES256 } from '@ionic-native/aes-256';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	private secureKey: string;
+	private secureIV: string;
 
-  }
+	constructor(
+		public navCtrl: NavController,
+		public platform : Platform,
+		private aes256: AES256
+		) 
+	{
+		this.generateSecureKeyAndIV(); 
+	}
+
+	async generateSecureKeyAndIV() {
+		this.secureKey = await this.aes256.generateSecureKey('random password 12345'); // Returns a 32 bytes string
+		this.secureIV = await this.aes256.generateSecureIV('random password 12345'); // Returns a 16 bytes string
+	}
+
+	encrypt() {
+		this.platform.ready().then(() => {
+			this.aes256.encrypt(this.secureKey, this.secureIV, 'test')
+			.then(res => console.log('Encrypted Data: ',res))
+			.catch((error: any) => console.error(error));
+		});
+	}
+
+	decrypt() {
+		this.platform.ready().then(() => {
+			this.aes256.decrypt(this.secureKey, this.secureIV, '3MeuaCC2th4MSto7Oyu4Xg==')
+			.then(res => console.log('Decrypted Data : ',res))
+			.catch((error: any) => console.error(error));
+		});
+	}
+
+	generateSecureKey() {
+		this.platform.ready().then(() => {
+			this.aes256.generateSecureKey('random password 12345')
+			.then(res => console.log('Secure Key : ',res))
+			.catch((error: any) => console.error(error));
+		});
+	}
+
+	generateSecureIV() {
+		this.platform.ready().then(() => {
+			this.aes256.generateSecureIV('random password 12345')
+			.then(res => console.log('Secure IV : ',res))
+			.catch((error: any) => console.error(error));
+		});
+	}
 
 }
